cc499b15a55d0f802883f3072de07d329d001e68
Vanessa Chan <chw.vanessa@gmail.com>
2020-10-10 13:33:04 -0400

    Tic Tac Toe (not completed)

--- __lecture/activity/tic-tac-toe/app.js
+++ __lecture/activity/tic-tac-toe/app.js
@@ -8,7 +8,7 @@ const endMessageDiv = document.querySelector('.js-end-message');
 const restartBtn = document.querySelector('.restart');
 
 // this array tracks the state of the game board.
-const game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 // Game Settings
 const PLAYER_1_ICON = 'X';
@@ -22,24 +22,68 @@ player1.classList.add('active');
 const handleRestart = () => {
   // reload the page
   // feels like cheating but it WILL reset the game
+  let allCells = document.querySelectorAll(".cell");
+  allCells.forEach(cell=>{
+    cell.innerText = "";
+  });
+
+  currentPlayer = '1';
+  player1.classList.add('active');
+  player2.classList.remove('active');
+  game = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 };
+
+restartBtn.addEventListener("click",handleRestart);
 
 const toggleRestartBtn = () => {
   // enable the restart btn
+  restartBtn.disabled =false;
 };
 
-const win = () => {
+const win = function() {
   // stop board from being clickable
   // print the winner to the screen
   // activate the restart btn
+  if(currentPlayer == '1'){
+    console.log(`The winner is: Player 1!`);
+  } else{
+    console.log(`The winner is: Player 2!`);
+  }
 };
 
-const verifyWin = () => {
+const verifyWin = function(){
   // Use the game array to determine the winner.
+
+  let newArray = game.slice('');
+  let [a,b,c,d,e,f,g,h,i] = newArray;
+
+  if(a==b && a==c || d==e && d==f ||g==h && g==i
+    || a==d && a==g || b==e && d==h ||c==f && c==i
+    || a==e && a==i || c==e && c==g ){
+
+    console.log("win!");
+    win;
+  }
+  else
+  {
+    console.log("not win yet");
+  }
+
 };
 
 const togglePlayer = () => {
   // use .active to show active player visually...
+  
+  // Change player
+  if(currentPlayer =='1'){
+    currentPlayer ='2';
+  }else{
+    currentPlayer ='1';
+  }
+
+  player2.classList.toggle('active');
+  player1.classList.toggle('active');
 };
 
 const handleClick = (event) => {
@@ -50,9 +94,16 @@ const handleClick = (event) => {
 
   if (typeof game[cellId] === 'number') {
     currentCellDiv.innerText = icon;
+    game[cellId] = icon;
 
-    // so much missing here...
+    verifyWin();
+
+
   }
+
+  togglePlayer();
 };
 
 board.addEventListener('click', handleClick);
+
+toggleRestartBtn();
